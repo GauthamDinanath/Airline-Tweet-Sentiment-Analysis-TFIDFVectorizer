cb4f9220e6201c489e229f5b01c5979e43c2e4c7
GauthamDinanath <64839751+GauthamDinanath@users.noreply.github.com>
2021-05-31 19:00:28 +0200

    updated

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+import tensorflow as tf
+from keras.preprocessing.sequence import pad_sequences
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,19 +12,20 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
 
-    data=request.form['tweet']
-    converted_data= vectorizer.transform([data])
-    prediction = model.predict(converted_data)
-    if prediction[0]==0:
-       emotion="positive" 
-    if prediction[0]==1:
-       emotion="neutral" 
-    if prediction[0]==2:
+    data=[request.form['tweet']]
+    tokenizer.fit_on_texts(data)
+    data = tokenizer.texts_to_sequences(data)
+    data = pad_sequences(data)
+    out=model.predict(data)
+    out=np.argmax(out,axis=1)[0]
+    if out==0:
        emotion="negative" 
+    if out==1:
+       emotion="positive" 
 
     return ('the sentiment is	' + emotion)
 
 if __name__ == "__main__":
-    model      =  pickle.load( open('model.pkl','rb'))
-    vectorizer =  pickle.load( open('vectorizer.pkl','rb'))
-    app.run(host='0.0.0.0',port=8080)
+    model=tf.keras.models.load_model('model_tf/my_model')
+    tokenizer =  pickle.load( open('model_tf/tokenizer.pkl','rb'))
+    app.run()
